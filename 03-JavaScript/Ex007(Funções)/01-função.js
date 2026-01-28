61bf11eec71946a7813f3e2b9cc15dc7e983b5e5
Joaquim Angelo <156119400+Joaquim-angelo@users.noreply.github.com>
2026-01-28 11:16:18 -0300

    Initial commit

---
+++ 03-JavaScript/Ex007(Funções)/01-função.js
@@ -0,0 +1,15 @@
+//exemplo de função em JS
+
+function paridade(n){
+    if(n%2==0){
+        return 'Par'
+    }else{
+        return 'Impar'
+    }
+}
+
+console.log(paridade(5))
+
+//Para rodar arquivos JS no console é necessária a instalação do node.js no computador.
+//Após isso deve-se instalar o code runner no vscode.
+//Para executar o script, basta selecionar run code ao clicar com botão direito no arquivo js.
